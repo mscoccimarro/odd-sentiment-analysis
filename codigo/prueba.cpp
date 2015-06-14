f1c1891d91ea01aa14c98f0b9a183860c503a711
patricia <pmachado194@gmail.com>
2015-06-14 19:47:49 -0300

    Se modifica el makefile para incluir prueba.cpp y realizar las pruebas correspondientes a la matriz de caracteristicas. Se elimina la prueba para obtener el set de entrenamiento.

---
+++ codigo/prueba.cpp
@@ -0,0 +1,10 @@
+#include "SetEntrenamiento.h"
+#include "ProcesadorSetEntrenamiento.h"
+#include "RandomForest.h"
+#include <iostream>
+using namespace std;
+
+int main (){
+	cout << "Iniciando prueba" << endl;
+	return 0;
+}
