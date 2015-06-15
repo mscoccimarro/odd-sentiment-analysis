0391343014a491e831468bd937179765b1dfae59
Makushi <maximiliano.scoccimarro@gmail.com>
2015-06-15 19:22:54 -0300

    Add wordSeparator with score and boost with ! signs. DRAFT CODE.

--- codigo/wordSeparator.cpp
+++ codigo/wordSeparator.cpp
@@ -8,6 +8,15 @@
 
 using namespace std;
 
+string getScale(double score) {
+	if(score < 0.2) return "muy malo";	
+	if(score >= 0.2 && score < 0.4) return "malo";	
+	if(score >= 0.4 && score < 0.6) return "regular";
+	if(score >= 0.6 && score < 0.8) return "bueno";
+	if(score >= 0.8) return "muy bueno";
+	return "score out of bounds";
+}
+
 int main (){
 	
 	vector<string> ids,contenido;
@@ -16,7 +25,9 @@ int main (){
 	ofstream out("words.txt");
 	ProcesadorSetEntrenamiento *pSetEntrenamiento = new ProcesadorSetEntrenamiento();
 	SetEntrenamiento *setEntrenamiento = pSetEntrenamiento->procesarSetEntrenamiento();
-	map<string, int> hash;	
+	map<string, int> positivos;	
+	map<string, int> negativos;
+
 	if (!setEntrenamiento->vacio()){
 		ids = setEntrenamiento->getIds();
 		i = ids.begin();
@@ -25,23 +36,42 @@ int main (){
 			contenido = setEntrenamiento->getContenido(*i);
 			j = contenido.begin();
 			while (j != contenido.end()){
-				string word = *j + "_" + sentiment;
-				if(hash[word])
-					hash[word]++;
-				else
-					hash[word] = 1;
+				string last_word, last_sentiment;
+				// save last word to sum possible "!" signs
+				if(*j != "!") {
+					last_word = *j + "_" + sentiment;
+					last_sentiment = sentiment;
+				}
+				if(last_sentiment == "0") {
+					if(negativos[last_word]) {
+						negativos[last_word]++;
+					} else {
+						negativos[last_word] = 1;
+					}
+				} else {
+					if(positivos[last_word]) {
+						positivos[last_word]++;
+					} else {
+						positivos[last_word] = 1;
+					}
+				}
 				j++;
 			}
 			i++;
 		}
-		map<int, string> reverse_map;
-		// Fill reverse map with hash
-		for(map<string, int>::iterator it = hash.begin(); it != hash.end(); it++) 
-			reverse_map[it->second] = it->first;
-		// Output "word appearances" => "word"_"sentiment"
-		// Ex: "1034 => hola_0"
-		for(map<int, string>::iterator it = reverse_map.begin(); it != reverse_map.end(); it++)
-			out << it->first << " => " << it->second << '\n';
+
+		double promedio;
+		for(map<string, int>::iterator it = positivos.begin(); it != positivos.end(); it++) {
+			// Si la palabra tiene apariciones negativas
+			string word = it->first.substr(0, it->first.length() - 2);
+			if(negativos[word + "_0"]) { 
+				promedio = static_cast<double>(it->second) / static_cast<double>((it->second + negativos[word + "_0"]));
+			} else 
+					promedio = 1;
+
+			out << word << " => " << promedio << " => " << getScale(promedio) << "\n";
+		}
+
 	}	
 	else cout << "Ocurrio un problema al intentar procesar el set de entrenamiento." << endl;	
 	delete pSetEntrenamiento;
