3a7ed2f6790a98c5283575584170f116bd69f7d3
patricia <pmachado194@gmail.com>
2015-06-14 22:52:22 -0300

    Se incorporan los signos de exclamacion como una palabra dentro del contenido de los reviews.

--- codigo/ProcesadorReviews.h
+++ codigo/ProcesadorReviews.h
@@ -9,6 +9,7 @@ class ProcesadorReviews {
 		std::vector <std::string> obtenerPalabras(std::string texto, int *resultado);
 
     private:
+		std::vector<std::string> procesarExclamaciones(std::string *palabra);
 		void limpiarTags(std::string *texto);
 		void eliminar_comillas_simples(std::string *texto);
     
