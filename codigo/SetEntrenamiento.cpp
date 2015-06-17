1ee090ae70bc110eccea72963a9766d8567c37c3
patricia <pmachado194@gmail.com>
2015-06-17 20:52:19 -0300

    Se agrega procesamiento del set de test.

--- codigo/SetEntrenamiento.cpp
+++ codigo/SetEntrenamiento.cpp
@@ -1,16 +1,13 @@
 #include "SetEntrenamiento.h"
 #include <string>
 #include <vector>
-#define CADENA_VACIA " "
 
 using namespace std;
 /* Agrega un review (con su id, sentimiento y contenido) al set de entrenamiento */
 void SetEntrenamiento::agregarReview(string id_review, string sentimiento_review, vector<string> contenido_review){
-	
 	this->listaIds.push_back(id_review);
 	this->reviews[id_review] = contenido_review;
 	this->sentimientos[id_review] = sentimiento_review;
-
 }
 /* Permite vaciar el set de entrenamiento */
 void SetEntrenamiento::vaciar(){
@@ -23,18 +20,6 @@ bool SetEntrenamiento::vacio(){
 	if (this->sentimientos.empty() && this->reviews.empty()) return true;
 	return false;	
 }
-/* Devuelve un vector con todas las ID's de los reviews del set de entrenamiento */
-vector<string> SetEntrenamiento::getIds(){
-	return this->listaIds;
-}
-
-/* Devuelve el vector de palabras que representa el contenido del review asociado al id recibido */
-vector<string> SetEntrenamiento::getContenido(string id_review){
-	if (!this->reviews.empty()) return this->reviews[id_review];
-	vector<string> vector_vacio;
-	return vector_vacio;
-}
-
 /* Devuelve el sentimiento del review asociado al id recibido */
 string SetEntrenamiento::getSentimiento(string id_review){
 	if (!this->sentimientos.empty()) return this->sentimientos[id_review];
--- codigo/SetEntrenamiento.h
+++ codigo/SetEntrenamiento.h
@@ -1,25 +1,15 @@
 #ifndef SET_ENTRENAMIENTO_H
 #define SET_ENTRENAMIENTO_H
-#include <string>
-#include <vector>
-#include <map>
-
-class SetEntrenamiento {
-    
+#include "SetReviews.h"
+class SetEntrenamiento: public SetReviews {
     public:
 		void agregarReview(std::string id_review, std::string sentimiento_review,std::vector<std::string> contenido_review);
-		std::vector<std::string> getIds();
-		std::vector<std::string> getContenido(std::string id_review);
 		std::string getSentimiento(std::string id_review);
 		bool vacio();
 		void vaciar();
-		
 	private:
-		std::vector<std::string> listaIds;
 		std::map<std::string,std::string> sentimientos;
-		std::map<std::string,std::vector<std::string> > reviews;
 };
-
 #endif
 
 
