2b2d536dcdc0defaef7383e350d9fa6db417c03d
Makushi <maximiliano.scoccimarro@gmail.com>
2015-06-19 18:17:41 -0300

    Add main file with args for traning and test files.

--- codigo/prueba.cpp
+++ codigo/prueba.cpp
@@ -2,9 +2,18 @@
 #include "ProcesadorSetEntrenamiento.h"
 #include "RandomForest.h"
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main (){
-	cout << "Iniciando prueba" << endl;
-	return 0;
+int main (int argc, char* argv[]){
+	string trainingFile, testFile;
+	
+	if(argc > 1) {
+		trainingFile = argv[1];
+	}
+
+	if(argc > 2) {
+		testFile = argv[2];
+	}
+
 }
