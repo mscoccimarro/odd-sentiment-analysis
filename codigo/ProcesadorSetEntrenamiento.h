99558cf470fbf84d02e1d3afd5f7eaac51aec1e2
Makushi <maximiliano.scoccimarro@gmail.com>
2015-06-20 10:09:32 -0300

    Add file parameters

--- codigo/ProcesadorSetEntrenamiento.h
+++ codigo/ProcesadorSetEntrenamiento.h
@@ -9,6 +9,7 @@
 class ProcesadorSetEntrenamiento : public ProcesadorSet {
     public:
 		ProcesadorSetEntrenamiento();
+		ProcesadorSetEntrenamiento(std::string fileName);
     private:
 		void mensaje_inicial();
 		void mensaje_final();
